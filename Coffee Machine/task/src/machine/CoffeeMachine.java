f310ddd792e5a2ad8e2127b3a427b2f100f7d658
Rizky Ardi Ilhami <rizky.ardi@pegipegi.com>
2022-11-19 03:39:01 +0700

    making coffee

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -2,6 +2,18 @@ package machine;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String[] outputs = new String[] {
+                "Starting to make a coffee",
+                "Grinding coffee beans",
+                "Boiling water",
+                "Mixing boiled water with crushed coffee beans",
+                "Pouring coffee into the cup",
+                "Pouring some milk into the cup",
+                "Coffee is ready!"
+        };
+
+        for (var output: outputs) {
+            System.out.println(output);
+        }
     }
 }
